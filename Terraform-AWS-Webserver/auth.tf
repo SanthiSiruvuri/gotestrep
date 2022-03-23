11a87e5d9038eea8702665ba6d908551090ddeee
SanthiSiruvuri <91609800+SanthiSiruvuri@users.noreply.github.com>
2022-03-23 11:40:31 +0530

    Update auth.tf

--- Terraform-AWS-Webserver/auth.tf
+++ Terraform-AWS-Webserver/auth.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIA56PAODQMNUONIRXO"
-    secret_key = "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"
-    region = "us-east-1"
+    access_key = 
+    secret_key = 
+    region = 
   
 }
